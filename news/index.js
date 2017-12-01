f2db0a51acc0ff6745dbe05d1774f2720c229032
nn <307585280@qq.com>
2017-12-01 16:22:59 +0800

    'asdf'

---
+++ news/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ms on 2017/12/1.
+ */
+
+$(function () {
+    let myScroll = new IScroll('#wrapper', {
+        scrollX: true,
+        scrollY: false,
+    });
+
+
+
+    $.ajax({
+        url:'http://api.jisuapi.com/news/channel?appkey=fe2a29e929bf5cc5',
+        dataType:'jsonp',
+        success:function (res) {
+            let arr=res.result;
+            let str=''
+            arr.forEach((val,index)=>{
+                if(index==0){
+                    str+=`<li class="active">${val}</li>`
+                }else{
+                    str+=`<li>${val}</li>`
+                }
+
+            })
+            $('#scroller ul').html(str);
+        }
+    })
+})
